51e053a100c75f96621a81e0c78ef9ad181c503d
Jeremy Miller <gkstaff@gmail.com>
2021-01-29 15:05:29 -0800

    completes the functionality of the website. GUI to be added next.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,13 @@
 //Business logic
-function Pizza(size, toppings) {
+function Pizza(size, toppings, total) {
   this.size = size;
   this.toppings = toppings;
-  this.total = 10;
+  this.total = total;
 }
 
 
 Pizza.prototype.calculateOrder = function() {
+  let baseCost = 10;
   // Checks for sizes and amount of toppings. Adds price value depending on size and amount of toppings
   if (this.size === "small") {
     this.total += 3;
@@ -15,32 +16,32 @@ Pizza.prototype.calculateOrder = function() {
   } else(this.size === "large")
   this.total += 5;
 
-  //the loop through toppings and adds 1 dollar for each to baseCost
+  //the loop through toppings and adds 1 dollar for each
   for (i = 0; i <= this.toppings.length; i++) {
     this.total += 1;
   }
-  return this.total;
+  return result = (this.total + baseCost);
 }
 
-function showResults() {
-  $("")
-  $("#showPizzaSize").text(size);
-  $("#showToppings").text(toppings);
-  $("#showTotal").text(toal);
-}
+
 
 
 
 //UI logic
+
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
-    var baseCost = 0;
+    total = 0;
     var size = $("input[name=pizzaSize]:checked").val();
     var toppings = $("input:checkbox[name=pizzaTopping]:checked").map(function() {
       return this.value;
     }).get();
-    let customer = new Pizza(size, toppings)
+    let customer = new Pizza(size, toppings, total)
     customer.calculateOrder();
+    $("#pizzaDisplay").show();
+    $("#showPizzaSize").text(size);
+    $("#showToppings").text(toppings);
+    $("#showTotal").text(result);
   });
 });
